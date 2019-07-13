74cc364605016ce66ad2fcb2f632233d7607a0fb
Graham Fairweather <xotic750@gmail.com>
2019-07-14 01:43:20 +0200

    Terraformed
    Automated build

--- src/number-to-decimal-form-string-x.js
+++ src/number-to-decimal-form-string-x.js
@@ -1,30 +1,29 @@
 /**
  * @file Convert a base-10 or scientific E-notation value to a decimal form string.
- * @version 1.0.1
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
+ * @version 1.0.1.
+ * @author Xotic750 <Xotic750@gmail.com>.
+ * @copyright  Xotic750.
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module number-to-decimal-form-string-x
+ * @module Number-to-decimal-form-string-x.
  */
 
-'use strict';
+const castNumber = require('cached-constructors-x').Number;
+const toStr = require('to-string-x');
 
-var castNumber = require('cached-constructors-x').Number;
-var toStr = require('to-string-x');
-var emptyString = '';
-var charAt = emptyString.charAt;
-var slice = emptyString.slice;
-var search = emptyString.search;
-var replace = emptyString.replace;
-var indexOf = emptyString.indexOf;
-var join = [].join;
-var decimalMark = '.';
-var hyphenMinus = '-';
-var zeroSymbol = '0';
-var minusZeroSymbol = '-0';
-var isValid = /^-?(?:(?:\d|[1-9]\d*)(?:\.\d+)?)(?:e[+-]?\d+)?$/i;
-var test = isValid.test;
-var errorMsg = 'not a valid base 10 numeric value';
+const emptyString = '';
+const {charAt} = emptyString;
+const {slice} = emptyString;
+const {search} = emptyString;
+const {replace} = emptyString;
+const {indexOf} = emptyString;
+const {join} = [];
+const decimalMark = '.';
+const hyphenMinus = '-';
+const zeroSymbol = '0';
+const minusZeroSymbol = '-0';
+const isValid = /^-?(?:(?:\d|[1-9]\d*)(?:\.\d+)?)(?:e[+-]?\d+)?$/i;
+const {test} = isValid;
+const errorMsg = 'not a valid base 10 numeric value';
 
 /**
  * This method converts a base-10 or scientific E-notation value to
@@ -36,7 +35,7 @@ var errorMsg = 'not a valid base 10 numeric value';
  * @throws {TypeError} If value is a Symbol or not coercible.
  * @returns {string} The value converted to a decimal form string.
  * @example
- * var toDecimalFormString = require('number-to-decimal-form-string-x');
+ * var toDecimalFormString = require('number-to-decimal-form-string-x');.
  *
  * toDecimalFormString(Number.MIN_SAFE_INTEGER); // '-9007199254740991'
  * toDecimalFormString(-0); // '-0'
@@ -74,20 +73,22 @@ var errorMsg = 'not a valid base 10 numeric value';
  */
 // eslint-disable-next-line complexity
 module.exports = function toDecimalFormString(value) {
-  var workingValue = value;
+  let workingValue = value;
 
   // Minus zero?
-  if (workingValue === 0 && (1 / workingValue) < 0) {
+  if (workingValue === 0 && 1 / workingValue < 0) {
     workingValue = minusZeroSymbol;
   } else {
     workingValue = toStr(workingValue);
+
     if (test.call(isValid, workingValue) === false) {
       throw new TypeError(errorMsg);
     }
   }
 
   // Determine sign.
-  var sign;
+  let sign;
+
   if (charAt.call(workingValue, 0) === hyphenMinus) {
     workingValue = slice.call(workingValue, 1);
     sign = -1;
@@ -96,14 +97,16 @@ module.exports = function toDecimalFormString(value) {
   }
 
   // Decimal point?
-  var pointIndex = indexOf.call(workingValue, decimalMark);
+  const pointIndex = indexOf.call(workingValue, decimalMark);
+
   if (pointIndex > -1) {
     workingValue = replace.call(workingValue, decimalMark, emptyString);
   }
 
-  var exponentIndex = pointIndex;
+  let exponentIndex = pointIndex;
   // Exponential form?
-  var index = search.call(workingValue, /e/i);
+  let index = search.call(workingValue, /e/i);
+
   if (index > 0) {
     // Determine exponent.
     if (exponentIndex < 0) {
@@ -117,15 +120,16 @@ module.exports = function toDecimalFormString(value) {
     exponentIndex = workingValue.length;
   }
 
-  var leadingZeroIndex = workingValue.length;
+  let leadingZeroIndex = workingValue.length;
   // Determine leading zeros.
   index = 0;
   while (index < leadingZeroIndex && charAt.call(workingValue, index) === zeroSymbol) {
     index += 1;
   }
 
-  var coefficient;
-  var exponent;
+  let coefficient;
+  let exponent;
+
   if (index === leadingZeroIndex) {
     // Zero.
     exponent = 0;
@@ -143,7 +147,7 @@ module.exports = function toDecimalFormString(value) {
     coefficient.length = leadingZeroIndex + 1;
 
     // Convert string to array of digits without leading/trailing zeros.
-    var position = 0;
+    let position = 0;
     while (index <= leadingZeroIndex) {
       coefficient[position] = castNumber(charAt.call(workingValue, index));
       position += 1;
@@ -151,8 +155,8 @@ module.exports = function toDecimalFormString(value) {
     }
   }
 
-  var decimalForm = join.call(coefficient, emptyString);
-  var decimalFormLength = decimalForm.length;
+  let decimalForm = join.call(coefficient, emptyString);
+  const decimalFormLength = decimalForm.length;
 
   if (exponent < 0) {
     exponent += 1;
@@ -164,6 +168,7 @@ module.exports = function toDecimalFormString(value) {
     decimalForm = zeroSymbol + decimalMark + decimalForm;
   } else if (exponent > 0) {
     exponent += 1;
+
     if (exponent > decimalFormLength) {
       exponent -= decimalFormLength;
       while (exponent) {
