a6bf5030ed00c81db8befb27b5e9f9b5bdbeabfe
Graham Fairweather <xotic750@gmail.com>
2019-07-22 00:41:44 +0200

    :bookmark: v2.0.9
    Auto build

--- dist/number-to-decimal-form-string-x.js
+++ dist/number-to-decimal-form-string-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-19T21:53:55.872Z",
+  "date": "2019-07-21T22:41:38.374Z",
   "describe": "",
   "description": "Convert a base-10 or scientific E-notation value to a decimal form string.",
   "file": "number-to-decimal-form-string-x.js",
   "hash": "fdc42172392c8a8ca64e",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
